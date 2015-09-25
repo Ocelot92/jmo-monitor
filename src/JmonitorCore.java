e9a7ce56b431763cc98f781f66509f448ff443eb
luca <cavalligianluca92@gmail.com>
2015-09-25 16:40:13 +0200

    Aggiunta classe JmonitorNode e sistemati alcuni metodi JmonitorCore

--- src/JmonitorCore.java
+++ src/JmonitorCore.java
@@ -9,16 +9,39 @@ import org.openstack4j.model.storage.object.options.ObjectPutOptions;
 import org.openstack4j.openstack.OSFactory;
 
 public class JmonitorCore {
-	private String OPENSTACK_URL_ENDPOINT;
-	private String CONTAINER_NAME;
+	private final String OPENSTACK_URL_ENDPOINT;
+	private final String SWIFT_CONTAINER_NAME;
 	private OSClient os;
-	private String SWIFT_CONTAINER_NAME;
-	private BlockingQueue ResultQueue; 
+	//The outputs of the plugins' scripts are stored in this queue waiting for being "consumed" by the os client
+	private BlockingQueue <InputStream> resultsQueue; 
 	
-	public JmonitorCore () {
+	public JmonitorCore (String endpoint, String container, BlockingQueue <InputStream> q, String user, String passwd, String tenant) {
+		OPENSTACK_URL_ENDPOINT = endpoint;
+		SWIFT_CONTAINER_NAME = container;
+		BlockingQueue <InputStream> ResultsQueue = q;
+		
+		//instance of the Openstack Client
+		os = OSFactory.builder()
+				.endpoint(endpoint)
+				.credentials(user,passwd)
+				.tenantName(tenant)
+				.authenticate();
 		
 	}
 	
+	public void StoreInSwift (){
+		os.objectStorage().containers().create(SWIFT_CONTAINER_NAME);
+		
+		try {
+			os.objectStorage().objects().put(SWIFT_CONTAINER_NAME,"cpu_hog.txt" ,
+					Payloads.create(resultsQueue.take()), 
+					ObjectPutOptions.create()
+						.path("/test")
+					);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 }
