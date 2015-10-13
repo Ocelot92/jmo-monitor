84531a9560608f901fb904fb496f60cf22fd0376
luca <cavalligianluca92@gmail.com>
2015-10-13 16:30:33 +0200

    class PluginClassLoader completed

--- src/PluginClassLoader.java
+++ src/PluginClassLoader.java
@@ -1,4 +1,47 @@
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class PluginClassLoader {
 
+public class PluginClassLoader extends ClassLoader {
+	File directory;
+	
+	public PluginClassLoader (File dir){
+		directory = dir;
+	}
+	
+	public Class<?> loadClass (String classname, boolean resolve){
+		Class<?> c = null;
+
+		try {
+			//check if the class is already loaded
+			c = findLoadedClass(classname);
+			//... otherwise check if it's a system class
+			if (c == null) {
+				c = findSystemClass(classname);
+				if ( c == null) {
+					//... otherwise load it from the plugin directory
+					
+					classname += ".class";
+					File f = new File (directory, classname);
+					int length = (int)f.length();
+					byte classbytes[] = new byte [length];
+					DataInputStream dis = new DataInputStream (new FileInputStream(f));
+					dis.readFully(classbytes);
+					dis.close();
+					c = defineClass(classname, classbytes, 0, length);
+				}
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return c;
+	}	
 }
--- src/PluginsManager.java
+++ src/PluginsManager.java
@@ -5,11 +5,27 @@ public class PluginsManager {
 	private String directory;
 	private List <IfcPlugin> plugins;
 	
-	public PluginsManager (File dir){
+	public PluginsManager (String dir){
 		directory = dir;
 	}
 	
 	public void loadPlugins (){
+		File dir = new File (System.getProperty("user.dir") + File.separator + directory);
+		ClassLoader cl = new PluginClassLoader(dir);
+		
+		if (dir.exists() && dir.isDirectory()) {
+			String files []= dir.list();
+			for (int i=0; i < files.length; i++){
+				if (files[i].endsWith(".class")) {
+					try {
+						Class<?> c = cl.loadClass(files[i].substring(0, files[i].indexOf('.')));
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+					
+				}
+			}
+		}
 		
 	}
 }
