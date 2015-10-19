b410eca469203d7a3432c03901db9e5c1b1a1d0c
luca <cavalligianluca92@gmail.com>
2015-10-19 11:30:37 +0200

    final(s) removed from IfcPlugin.

--- src/org/jmonitorstack/IfcPlugin.java
+++ src/org/jmonitorstack/IfcPlugin.java
@@ -2,24 +2,22 @@ package org.jmonitorstack;
 import java.util.concurrent.BlockingQueue;
 
 abstract public class IfcPlugin {
-	private final String NAME;
-	private final long RATE;
-	private final BlockingQueue<JmonitorNode> monitorQueue;
+	protected String name;
+	protected long rate;
+	protected BlockingQueue<JmonitorNode> monitorQueue;
 	
 	public IfcPlugin (BlockingQueue<JmonitorNode> q) {
 		monitorQueue = q;
-		NAME = null;
-		RATE = 0;
 	}
 	
 	abstract public void monitoring ();
 	
 	public String getName() {
-		return NAME;
+		return name;
 	}
 	
 	public long getRate() {
-		return RATE;
+		return rate;
 	}
 	
 //Use this method to set the plugin name and the rate at which call the monitoring() method
